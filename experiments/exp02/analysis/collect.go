a587f467cd7911a48929cfcb7a48a16592fa68ea
Drausin Wulsin <daedalus2718@gmail.com>
2018-04-24 23:49:19 -0400

    WIP collect

--- experiments/exp02/analysis/collect.go
+++ experiments/exp02/analysis/collect.go
@@ -1,24 +1,25 @@
-package analysis
+package main
 
 import (
-	"net/http"
-	"time"
+	"fmt"
 	"log"
-	"io/ioutil"
+	"net/http"
 )
 
 var queries = map[string]string{
-	"get.p95": `histogram_quantile(0.95, sum(rate(grpc_server_handling_seconds_bucket{grpc_type="unary",grpc_method="Get"}[5m])) by (pod_name, le)) * 1000`
-	"get.p50": `histogram_quantile(0.95, sum(rate(grpc_server_handling_seconds_bucket{grpc_type="unary",grpc_method="Get"}[5m])) by (pod_name, le)) * 1000`
+	"get.p95":          getQuantileQuery("Get", 0.95),
+	"get.p50":          getQuantileQuery("Get", 0.50),
+	"put.p95":          getQuantileQuery("Put", 0.95),
+	"put.p50":          getQuantileQuery("Put", 0.50),
 	"peer_query_count": `libri_goodwill_peer_query_count{outcome="SUCCESS"}`,
 }
 
 func main() {
 	queryRangeURL := "http://prometheus.default.svc.cluster.local:9090/api/v1/query_range"
-	startTime := "2018-04-21T18:30:00Z"
-	endTime := "2018-04-21T19:00:00Z"
+	startTime := "2018-04-25T02:30:00Z"
+	endTime := "2018-04-25T03:30:00Z"
 	step := "1m"
-	client := http.Client{Timeout: 20 * time.Second}
+	//client := http.Client{Timeout: 20 * time.Second}
 
 	for label, query := range queries {
 		rq, err := http.NewRequest(http.MethodGet, queryRangeURL, nil)
@@ -30,16 +31,31 @@ func main() {
 		q.Add("start", startTime)
 		q.Add("end", endTime)
 		q.Add("step", step)
+		log.Printf("%s : %s?%s\n", label, rq.URL.String(), q.Encode())
 
-		rp, err := client.Do(rq)
-		if err != nil {
-			log.Fatal(err)
-		}
-		bodyBytes, err := ioutil.ReadAll(rp.Body)
-		rp.Body.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
+		//rp, err := client.Do(rq)
+		//if err != nil {
+		//	log.Fatal(err)
+		//}
+		//bodyBytes, err := ioutil.ReadAll(rp.Body)
+		//rp.Body.Close()
+		//if err != nil {
+		//	log.Fatal(err)
+		//}
+		//filename := label + ".json"
+		//err = ioutil.WriteFile(filename, bodyBytes, 0644)
+		//if err != nil {
+		//	log.Fatal(err)
+		//}
+		//log.Printf("wrote %s\n", filename)
 	}
 }
 
+func getQuantileQuery(endpoint string, quantile float32) string {
+	return fmt.Sprintf(
+		`histogram_quantile(%f, sum(rate(grpc_server_handling_seconds_`+
+			`bucket{grpc_type="unary",grpc_method="%s"}[5m])) by (pod_name, le)) * 1000`,
+		quantile,
+		endpoint,
+	)
+}
