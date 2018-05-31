864ebff48af70a3c882246f88542c219778f40bd
Drausin Wulsin <daedalus2718@gmail.com>
2018-05-30 22:38:08 -0400

    Revert "fix flags for up & down libs (#18)"
    This reverts commit 8a6e62af41d351fe13431bf99528c65a2158f62a.

--- pkg/cmd/run.go
+++ pkg/cmd/run.go
@@ -41,9 +41,9 @@ func init() {
 
 	runCmd.Flags().StringSliceP(librariansFlag, "a", nil,
 		"comma-separated addresses (IPv4:Port) of librarian(s)")
-	runCmd.Flags().StringSlice(upLibrariansFlag, nil,
+	runCmd.Flags().StringSliceP(upLibrariansFlag, "a", nil,
 		"comma-separated addresses (IPv4:Port) of upload librarian(s)")
-	runCmd.Flags().StringSlice(downLibrariansFlag, nil,
+	runCmd.Flags().StringSliceP(downLibrariansFlag, "a", nil,
 		"comma-separated addresses (IPv4:Port) of download librarian(s)")
 	runCmd.Flags().Duration(durationFlag, sim.DefaultDuration,
 		"experiment duration")
