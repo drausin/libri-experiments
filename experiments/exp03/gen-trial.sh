e5829ea6e013db79b0cd69e159dfa15a56ad26fa
Drausin Wulsin <daedalus2718@gmail.com>
2018-06-02 23:58:25 -0400

    init exp03 stuff

---
+++ experiments/exp03/gen-trial.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+TRIALS_DIR="${DIR}/trials"
+LIBRI_CLOUD_DIR="${HOME}/.go/src/github.com/drausin/libri/deploy/cloud"
+EXP_NUM="03"
+GCP_BUCKET="libri-clusters"
+GCP_PROJECT="libri-170711"
+export TF_VAR_credentials_file="${HOME}/.gcloud/keys/libri.experimeter.json"
+
+TRIAL_NUM="${1}"
+CLUSTER_NAME="exp${EXP_NUM}-trial${TRIAL_NUM}"
+CLUSTER_DIR="${DIR}/trials/trial${TRIAL_NUM}"
+
+pushd ${LIBRI_CLOUD_DIR} >/dev/null 2>&1
+mkdir -p "${CLUSTER_DIR}"
+go run cluster.go init gcp \
+    --flagsFilepath "${DIR}/trials/template/terraform.tfvars" \
+    --clusterDir "${CLUSTER_DIR}" \
+    --clusterName "${CLUSTER_NAME}" \
+    --bucket "${GCP_BUCKET}" \
+    --gcpProject "${GCP_PROJECT}"
+
+pushd "${DIR}/../../deploy/cloud" >/dev/null 2>&1
+go run gen.go -e "${CLUSTER_DIR}/terraform.tfvars" -d ${CLUSTER_DIR}
+popd >/dev/null 2>&1
+
+echo -e "cluster ${CLUSTER_NAME} initialized successfully; run the following to start trial\n"
+echo "  pushd ${LIBRI_CLOUD_DIR} && go run cluster.go apply --clusterDir ${CLUSTER_DIR}"
+
+popd >/dev/null 2>&1
---
+++ experiments/exp03/trials/collect.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+const timeWindow = "30m"
+
+var (
+	bytesStoredQueries = map[string]string{
+		"bytes.stored.peer":        `sum by (pod_name)(grpc_server_doc_stored_size{})`,
+		"bytes.stored.cluster":     `sum by ()(grpc_server_doc_stored_size{})`,
+		"bytes.store-rate.peer":    fmt.Sprintf(`sum by (pod_name)(rate( grpc_server_doc_stored_size{}[%s]))`, timeWindow),
+		"bytes.store-rate.cluster": fmt.Sprintf(`sum by ()(rate( grpc_server_doc_stored_size{}[%s]))`, timeWindow),
+	}
+
+	docsStoredQueries = map[string]string{
+		"docs.stored.peer":        `sum by (pod_name)(grpc_server_doc_stored_count{})`,
+		"docs.stored.cluster":     `sum by ()(grpc_server_doc_stored_count{})`,
+		"docs.store-rate.peer":    fmt.Sprintf(`sum by (pod_name)(rate( grpc_server_doc_stored_count{}[%s]))`, timeWindow),
+		"docs.store-rate.cluster": fmt.Sprintf(`sum by ()(rate( grpc_server_doc_stored_count{}[%s]))`, timeWindow),
+	}
+	methods        = []string{"Put", "Get"}
+	quantiles      = []float64{0.5, 0.95}
+	latencyQueries = getLatencyQueries(quantiles, methods)
+	qpsQueries     = getQPSQueries(methods)
+
+	grafanaPodName = ""
+	outDir         = ""
+	queryRangeURL  = "http://prometheus.default.svc.cluster.local:9090/api/v1/query"
+	cmd            = &cobra.Command{
+		Use:   "collect",
+		Short: "operate a librarian server, a peer in the libri network",
+		Long:  `TODO (drausin) add longer description and examples here`,
+		Run: func(cmd *cobra.Command, args []string) {
+			queries := make(map[string]string)
+			appendQueries(queries, bytesStoredQueries)
+			appendQueries(queries, docsStoredQueries)
+			appendQueries(queries, latencyQueries)
+			appendQueries(queries, qpsQueries)
+
+			for label, query := range queries {
+				rq, err := http.NewRequest(http.MethodGet, queryRangeURL, nil)
+				if err != nil {
+					log.Fatal(err)
+				}
+				q := rq.URL.Query()
+				q.Add("query", query)
+				fmt.Printf("kubectl exec %s -- curl '%s?%s' | gzip > %s/%s.json.gz\n",
+					grafanaPodName, rq.URL.String(), q.Encode(), outDir, label)
+			}
+		},
+	}
+)
+
+func init() {
+	cmd.Flags().StringVar(&grafanaPodName, "grafanaPod", "", "Grafana pod name")
+	cmd.Flags().StringVar(&outDir, "outDir", "", "output directory")
+}
+
+func main() {
+	cmd.Execute()
+}
+
+func appendQueries(a, b map[string]string) {
+	for label, query := range b {
+		a[label] = query
+	}
+}
+
+func getLatencyQueries(quantiles []float64, methods []string) map[string]string {
+	qs := make(map[string]string)
+	for _, quantile := range quantiles {
+		for _, method := range methods {
+			peerName := fmt.Sprintf("%s.p%d.peer", method, int(quantile*100))
+			qs[peerName] = fmt.Sprintf(`histogram_quantile(%f, 
+sum(rate(grpc_server_handling_seconds_bucket{grpc_type="unary",grpc_method="%s"}[%s])) by (pod_name, le)) * 1000`, quantile, method, timeWindow)
+			clusterName := fmt.Sprintf("%s.p%d.cluster", method, int(quantile*100))
+			qs[clusterName] = fmt.Sprintf(`histogram_quantile(%f, 
+sum(rate(grpc_server_handling_seconds_bucket{grpc_type="unary",grpc_method="%s"}[%s])) by (le)) * 1000`, quantile, method, timeWindow)
+		}
+	}
+	return qs
+}
+
+func getQPSQueries(methods []string) map[string]string {
+	qs := make(map[string]string)
+	qs["all.qps.peer"] = fmt.Sprintf(`sum by (pod_name)(rate( grpc_server_handled_total{}[%s] ))`, timeWindow)
+	qs["all.qps.cluster"] = fmt.Sprintf(`sum by ()(rate( grpc_server_handled_total{}[%s] ))`, timeWindow)
+	for _, method := range methods {
+		peerName := fmt.Sprintf("%s.qps.peer", method)
+		qs[peerName] = fmt.Sprintf(`sum by (pod_name)(rate( grpc_server_handled_total{grpc_method="%s"}[%s] ))`, method, timeWindow)
+		allName := fmt.Sprintf("%s.qps.cluster", method)
+		qs[allName] = fmt.Sprintf(`sum by ()(rate( grpc_server_handled_total{grpc_method="%s"}[%s] ))`, method, timeWindow)
+	}
+	return qs
+}
