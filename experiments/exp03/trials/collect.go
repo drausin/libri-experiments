5433ebde5210388887ec118ec305399e86605981
Drausin Wulsin <daedalus2718@gmail.com>
2018-07-14 23:25:09 -0400

    fix lint

--- experiments/exp03/trials/collect.go
+++ experiments/exp03/trials/collect.go
@@ -12,17 +12,29 @@ const timeWindow = "30m"
 
 var (
 	bytesStoredQueries = map[string]string{
-		"bytes.stored.peer":        `sum by (pod_name)(grpc_server_doc_stored_size{})`,
-		"bytes.stored.cluster":     `sum by ()(grpc_server_doc_stored_size{})`,
-		"bytes.store-rate.peer":    fmt.Sprintf(`sum by (pod_name)(rate( grpc_server_doc_stored_size{}[%s]))`, timeWindow),
-		"bytes.store-rate.cluster": fmt.Sprintf(`sum by ()(rate( grpc_server_doc_stored_size{}[%s]))`, timeWindow),
+		"bytes.stored.peer":    `sum by (pod_name)(grpc_server_doc_stored_size{})`,
+		"bytes.stored.cluster": `sum by ()(grpc_server_doc_stored_size{})`,
+		"bytes.store-rate.peer": fmt.Sprintf(
+			`sum by (pod_name)(rate( grpc_server_doc_stored_size{}[%s]))`,
+			timeWindow,
+		),
+		"bytes.store-rate.cluster": fmt.Sprintf(
+			`sum by ()(rate( grpc_server_doc_stored_size{}[%s]))`,
+			timeWindow,
+		),
 	}
 
 	docsStoredQueries = map[string]string{
-		"docs.stored.peer":        `sum by (pod_name)(grpc_server_doc_stored_count{})`,
-		"docs.stored.cluster":     `sum by ()(grpc_server_doc_stored_count{})`,
-		"docs.store-rate.peer":    fmt.Sprintf(`sum by (pod_name)(rate( grpc_server_doc_stored_count{}[%s]))`, timeWindow),
-		"docs.store-rate.cluster": fmt.Sprintf(`sum by ()(rate( grpc_server_doc_stored_count{}[%s]))`, timeWindow),
+		"docs.stored.peer":    `sum by (pod_name)(grpc_server_doc_stored_count{})`,
+		"docs.stored.cluster": `sum by ()(grpc_server_doc_stored_count{})`,
+		"docs.store-rate.peer": fmt.Sprintf(
+			`sum by (pod_name)(rate( grpc_server_doc_stored_count{}[%s]))`,
+			timeWindow,
+		),
+		"docs.store-rate.cluster": fmt.Sprintf(
+			`sum by ()(rate( grpc_server_doc_stored_count{}[%s]))`,
+			timeWindow,
+		),
 	}
 	methods        = []string{"Put", "Get"}
 	quantiles      = []float64{0.5, 0.95}
@@ -49,8 +61,10 @@ var (
 				}
 				q := rq.URL.Query()
 				q.Add("query", query)
-				fmt.Printf("kubectl exec %s -- curl '%s?%s' | gzip > %s/%s.json.gz\n",
-					grafanaPodName, rq.URL.String(), q.Encode(), outDir, label)
+				fmt.Printf(
+					"kubectl exec %s -- curl '%s?%s' | gzip > %s/%s.json.gz\n",
+					grafanaPodName, rq.URL.String(), q.Encode(), outDir, label,
+				)
 			}
 		},
 	}
@@ -62,7 +76,10 @@ func init() {
 }
 
 func main() {
-	cmd.Execute()
+	err := cmd.Execute()
+	if err != nil {
+		panic(err)
+	}
 }
 
 func appendQueries(a, b map[string]string) {
@@ -76,11 +93,17 @@ func getLatencyQueries(quantiles []float64, methods []string) map[string]string 
 	for _, quantile := range quantiles {
 		for _, method := range methods {
 			peerName := fmt.Sprintf("%s.p%d.peer", method, int(quantile*100))
-			qs[peerName] = fmt.Sprintf(`histogram_quantile(%f, 
-sum(rate(grpc_server_handling_seconds_bucket{grpc_type="unary",grpc_method="%s"}[%s])) by (pod_name, le)) * 1000`, quantile, method, timeWindow)
+			metric := fmt.Sprintf(`grpc_server_handling_seconds_bucket{
+grpc_type="unary",grpc_method="%s"}`, method)
+			qs[peerName] = fmt.Sprintf(
+				`histogram_quantile(%f,sum(rate(%s[%s])) by (pod_name, le)) * 1000`,
+				quantile, metric, timeWindow,
+			)
 			clusterName := fmt.Sprintf("%s.p%d.cluster", method, int(quantile*100))
-			qs[clusterName] = fmt.Sprintf(`histogram_quantile(%f, 
-sum(rate(grpc_server_handling_seconds_bucket{grpc_type="unary",grpc_method="%s"}[%s])) by (le)) * 1000`, quantile, method, timeWindow)
+			qs[clusterName] = fmt.Sprintf(
+				`histogram_quantile(%f,sum(rate(%s[%s])) by (le)) * 1000`,
+				quantile, metric, timeWindow,
+			)
 		}
 	}
 	return qs
@@ -88,13 +111,20 @@ sum(rate(grpc_server_handling_seconds_bucket{grpc_type="unary",grpc_method="%s"}
 
 func getQPSQueries(methods []string) map[string]string {
 	qs := make(map[string]string)
-	qs["all.qps.peer"] = fmt.Sprintf(`sum by (pod_name)(rate( grpc_server_handled_total{}[%s] ))`, timeWindow)
-	qs["all.qps.cluster"] = fmt.Sprintf(`sum by ()(rate( grpc_server_handled_total{}[%s] ))`, timeWindow)
+	qs["all.qps.peer"] = fmt.Sprintf(
+		`sum by (pod_name)(rate( grpc_server_handled_total{}[%s] ))`,
+		timeWindow,
+	)
+	qs["all.qps.cluster"] = fmt.Sprintf(
+		`sum by ()(rate( grpc_server_handled_total{}[%s] ))`,
+		timeWindow,
+	)
 	for _, method := range methods {
 		peerName := fmt.Sprintf("%s.qps.peer", method)
-		qs[peerName] = fmt.Sprintf(`sum by (pod_name)(rate( grpc_server_handled_total{grpc_method="%s"}[%s] ))`, method, timeWindow)
+		metric := fmt.Sprintf(`grpc_server_handled_total{grpc_method="%s"}`, method)
+		qs[peerName] = fmt.Sprintf(`sum by (pod_name)(rate(%s[%s]))`, metric, timeWindow)
 		allName := fmt.Sprintf("%s.qps.cluster", method)
-		qs[allName] = fmt.Sprintf(`sum by ()(rate( grpc_server_handled_total{grpc_method="%s"}[%s] ))`, method, timeWindow)
+		qs[allName] = fmt.Sprintf(`sum by ()(rate(%s[%s]))`, metric, timeWindow)
 	}
 	return qs
 }
